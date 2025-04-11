e2b5fe076ba8789839597310a0c1dd7eaa3cd024
Shiburaj Pappu <shiburaj@eng.rizvi.edu.in>
2025-04-11 21:46:53 +0530

    exp13 and 14

---
+++ exp12/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname+"/pages/index.html");
+});
+
+app.get("/skills", (req,res) => {
+    res.sendFile(__dirname+"/pages/my_skills.html");
+});
+
+
+app.listen(port, (err) => {
+    console.log("Server started on http://localhost:"+port);
+})
